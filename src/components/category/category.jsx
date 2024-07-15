6b4731d93eb16ca92e82f06542cca942ac80a21d
puzika <tolibjonlatipovk@gmail.com>
2024-07-15 12:42:35 +0700

    styled category description and added redirection to category cards

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import './category.scss';
 
 export default function Category({category}) {
+   const navigate = useNavigate();
+
+   function handleClick() {
+      navigate(`/shop/${category.name}`);
+   }
+
    return (
-      <li className="category" style={{backgroundImage: `url(${category.imageUrl})`}}>
+      <li onClick={handleClick} className="category" style={{backgroundImage: `url(${category.imageUrl})`}}>
          <div className="category__description">
             <h3 className='category__heading'>{category.name}</h3>
-            <a className='category__link' href="#">Shop now</a>
+            <a className='category__link' href={`/shop/${category.name}`}>Shop now</a>
          </div>
       </li>
    )
